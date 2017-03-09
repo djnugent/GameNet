48d1adb0889a733143f2b6fe4e272e81b30a3195
djnugent <djnugent@iastate.edu>
2017-03-08 19:19:53 -0600

    Updated docker

--- start-docker.sh
+++ start-docker.sh
@@ -3,5 +3,6 @@ docker run --privileged --rm -it\
     -v /root/.docker:/root/.docker \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -e DOCKER_NET_HOST=172.17.0.1 \
+    -p 12345:12345 \
     -v `pwd`:/srv/ \
-    universelite
+    universe
